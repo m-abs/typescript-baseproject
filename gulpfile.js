4f19ccc13aafefbd088084fb09e63a4a5936caa9
Morten Anton Bach Sjøgren <msn@nota.dk>
2016-04-20 14:06:39 +0200

    Generalize fileGlobs in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,51 +7,65 @@ const sourcemaps = require('gulp-sourcemaps');
 
 const builtDir = 'lib';
 const testBuildDir = '.tmp';
+
+const srcDir = 'src';
+const testSrcDir = 'tests';
+
+const fileGlobs = {
+  'lib.d.ts': `${builtDir}/**/*.d.ts`,
+  'src.ts': `${srcDir}/**/*.ts`,
+  'test.ts': `${testSrcDir}/**/*.ts`,
+  'test.build.js': `${testBuildDir}/**/*.js`,
+  'refs': `references.d.ts`
+};
+
 const ts = require('gulp-typescript');
+const typescript = require('typescript');
+const tslint = require('gulp-tslint');
+const del = require('del');
+const mergeStream = require('merge-stream');
 
 gulp.task('clean', function() {
-  const del = require('del');
   del.sync([
     builtDir,
     testBuildDir,
-    'daisyparser.js',
-    'daisyparser.d.ts'
   ], {
     force: true
   });
 });
 
 gulp.task('lint', function() {
-  const tslint = require('gulp-tslint');
   return gulp.src([
-    'src/**/*.ts',
-    'tests/**/.ts'
+    fileGlobs['src.ts'],
+    fileGlobs['test.ts'],
   ])
     .pipe(tslint())
-    .pipe(tslint.report('verbose'));
+    .pipe(tslint.report('verbose', {
+      emitError: false
+    }));
 });
 
 gulp.task('tsc', ['lint'], function() {
   const tsProject = ts.createProject('tsconfig.json', {
     sortOutput: true,
-    typescript: require('typescript')
+    typescript,
   });
 
   const tsFiles = gulp.src([
-    'src/**/*.ts',
-    'references.d.ts'
+    fileGlobs['src.ts'],
+    fileGlobs['refs'],
   ])
     .pipe(sourcemaps.init())
     .pipe(ts(tsProject));
 
   const jsFiles = tsFiles.js
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./lib'));
+    .pipe(gulp.dest(builtDir));
 
   const dtsFiles = tsFiles.dts
-    .pipe(gulp.dest('./lib'));
+    .pipe(gulp.dest(builtDir));
 
-  return require('merge-stream')(jsFiles, dtsFiles);
+  return mergeStream(jsFiles, dtsFiles);
 });
 
 gulp.task('build', [
@@ -62,20 +76,22 @@ gulp.task('build', [
 
 gulp.task('test-build', ['build'], function() {
   const tsProject = ts.createProject('tests/tsconfig.json', {
-    typescript: require('typescript')
+    typescript,
   });
 
   return gulp.src([
-    'libs/**/*.d.ts',
-    'tests/**/*.ts',
-    'references.d.ts'
+    fileGlobs['lib.d.ts'],
+    fileGlobs['test.ts'],
+    fileGlobs['refs'],
   ])
     .pipe(ts(tsProject))
     .pipe(gulp.dest(testBuildDir));
 });
 
 gulp.task('test', ['test-build'], function() {
-  return gulp.src(`${testBuildDir}/**/*.js`)
+  return gulp.src([
+    fileGlobs['test.build.js'],
+  ])
     .pipe(mocha({
       require: ['source-map-support/register']
     }));
@@ -85,11 +101,11 @@ gulp.task('watch', ['test-build'], function() {
   gulp.watch([
     'tsconfig.json',
     'tslint.json',
-    'src/**/*.ts',
     'tests/tsconfig.json',
-    'tests/**/*.ts',
-    'references.d.ts'
+    fileGlobs['src.ts'],
+    fileGlobs['test.ts'],
+    fileGlobs['refs'],
   ], ['test']);
 });
 
-// gulp.task('default', ['watch', 'build', 'test']);
+gulp.task('default', ['watch']);
