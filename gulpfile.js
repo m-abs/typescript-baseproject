0b416b9ac2a10589c23c8084551d73fa4b41323d
Morten Anton Bach Sjøgren <msn@nota.dk>
2016-05-09 12:16:58 +0200

    Add tsconfig-glob support. tsconfig.json files array is now updated from the filesGlob like in atom

--- gulpfile.js
+++ gulpfile.js
@@ -1,115 +1,183 @@
 'use strict';
 
+// Imports
 const pkg = require('./package.json');
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
-const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
-
-const builtDir = 'lib';
-const testBuildDir = '.tmp';
-
-const srcDir = 'src';
-const testSrcDir = 'tests';
-
-const fileGlobs = {
-  'lib.d.ts': `${builtDir}/**/*.d.ts`,
-  'src.ts': `${srcDir}/**/*.ts`,
-  'test.ts': `${testSrcDir}/**/*.ts`,
-  'test.build.js': `${testBuildDir}/**/*.js`,
-  'refs': `references.d.ts`
-};
-
 const ts = require('gulp-typescript');
 const typescript = require('typescript');
 const tslint = require('gulp-tslint');
 const del = require('del');
 const mergeStream = require('merge-stream');
+const tsConfigGlob = require('tsconfig-glob');
 
+// Const values
+const buildDir = 'lib';
+const testBuildDir = '.tmp';
+
+const srcDir = 'src';
+const testSrcDir = 'tests';
+
+// Helper functions:
+
+/**
+ * Lint source ts-files
+ * @param tsconfigPath    path to tsconfig.json-file
+ */
+const lint = function(tsconfigPath) {
+  const tsProject = ts.createProject(tsconfigPath, {
+    typescript,
+  });
+
+  return tsProject.src()
+    .pipe(tslint())
+    .pipe(tslint.report('verbose', {
+      emitError: false
+    }));
+};
+
+/**
+ * @param configPath      path containing the tsconfig.json-file
+ * @param cb              The task's callback function, this is required to avoid infinite loops
+ */
+const filesGlob = function(configPath, cb) {
+  return tsConfigGlob({
+    configPath,
+    cwd: process.cwd(),
+    indent: 2
+  }, function (err) {
+    if (err) {
+      console.log('[tsConfig - err] ' + JSON.stringify(err));
+      throw err;
+    }
+
+    cb();
+  });
+};
+
+// Tasks:
+
+// Delete transpiled files from the built-dir and the test-built-dir
 gulp.task('clean', function() {
   del.sync([
-    builtDir,
+    buildDir,
     testBuildDir,
   ], {
     force: true
   });
 });
 
-gulp.task('lint', function() {
-  return gulp.src([
-    fileGlobs['src.ts'],
-    fileGlobs['test.ts'],
-  ])
-    .pipe(tslint())
-    .pipe(tslint.report('verbose', {
-      emitError: false
-    }));
+// Lint project files
+gulp.task('lint', ['tsconfig:filesGlob'], function() {
+  return lint('tsconfig.json');
 });
 
+// Lint test files
+// Depends:
+//  - tsc => Because tests imports the transpiled files from ./lib and not the source files
+//  - tsconfig:tests:filesGlob
+gulp.task('lint:tests', ['tsc', 'tsconfig:tests:filesGlob'], function() {
+  return lint('tests/tsconfig.json');
+});
+
+// Transpil project src-files into JavaScript
 gulp.task('tsc', ['lint'], function() {
+  // Load projects config
   const tsProject = ts.createProject('tsconfig.json', {
-    sortOutput: true,
+    sortOutput: true, // This is required for the sourcemap to work
     typescript,
   });
 
-  const tsFiles = gulp.src([
-    fileGlobs['src.ts'],
-    fileGlobs['refs'],
-  ])
+  // Setup building the ts-files
+  const tsFiles = tsProject.src()
     .pipe(sourcemaps.init())
     .pipe(ts(tsProject));
 
+  // In gulp-typescript, .js-files and .d.ts-files end up in separate output streams
+
+  // Write the JavaScript-files to our buildDir
   const jsFiles = tsFiles.js
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(builtDir));
+    .pipe(gulp.dest(buildDir));
 
+  // Write the typedefition-files (.d.ts-files) to our buildDir
   const dtsFiles = tsFiles.dts
-    .pipe(gulp.dest(builtDir));
+    .pipe(gulp.dest(buildDir));
 
+  // Merge the two streams for completing this task
   return mergeStream(jsFiles, dtsFiles);
 });
 
+// Wrapper task for building the project
 gulp.task('build', [
   'clean',
   'lint',
   'tsc'
 ]);
 
-gulp.task('test-build', ['build'], function() {
+// Transpil project tests into JavaScript-files for mocha tests below
+gulp.task('tsc:tests', ['build', 'lint:tests'], function() {
   const tsProject = ts.createProject('tests/tsconfig.json', {
     typescript,
-  });
+  })
 
-  return gulp.src([
-    fileGlobs['lib.d.ts'],
-    fileGlobs['test.ts'],
-    fileGlobs['refs'],
-  ])
+  return tsProject.src()
     .pipe(ts(tsProject))
-    .pipe(rename(function(path) {
-      path.dirname = path.dirname.replace(/^tests\/?/, '');
-    }))
     .pipe(gulp.dest(testBuildDir));
 });
 
-gulp.task('test', ['test-build'], function() {
+// Run the mocha-tests
+// Depends:
+//  - tsc:tests => For transpiling the test source-code
+gulp.task('test', ['tsc:tests'], function() {
+  // Mocha works on the JavaScript-files not the .ts-files
   return gulp.src([
-    fileGlobs['test.build.js'],
+    `${testBuildDir}/**/*.js`,
   ])
     .pipe(mocha({
       require: ['source-map-support/register']
     }));
 });
 
-gulp.task('watch', ['test-build'], function() {
-  gulp.watch([
+// Watch for changes
+gulp.task('watch', function() {
+  const watchFilesGlob = [
     'tsconfig.json',
-    'tslint.json',
     'tests/tsconfig.json',
-    fileGlobs['src.ts'],
-    fileGlobs['test.ts'],
-    fileGlobs['refs'],
-  ], ['test']);
+    'tslint.json',
+    'typings.json',
+  ];
+
+  // Load the filesGlob from the two tsconfig.json-files, so we can watch them.
+  for (const glob of require('./tsconfig.json').filesGlob) {
+    watchFilesGlob.push(glob);
+  }
+
+  for (const glob of require('./tests/tsconfig.json').filesGlob) {
+    watchFilesGlob.push(`tests/${glob}`);
+  }
+
+  return gulp.watch(watchFilesGlob, {
+    readDelay: 250
+  }, ['test']);
 });
 
+// Make watch-task our default task
 gulp.task('default', ['watch']);
+
+// Update tsconfig.json's files-array from the filesGlob
+gulp.task('tsconfig:filesGlob', function(cb) {
+  return filesGlob('.', cb);
+});
+
+// Update tests/tsconfig.json's files-array from the filesGlob
+gulp.task('tsconfig:tests:filesGlob', function(cb) {
+  return filesGlob('./tests', cb);
+});
+
+// Helper task for updating tsconfig.json and tests/tsconfig.json
+gulp.task('filesGlob', [
+  'tsconfig:filesGlob',
+  'tsconfig:tests:filesGlob'
+]);
